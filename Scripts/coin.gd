7454f5e495406a9fcb9ee569ca4838dffc88521e
RizalAnas00 <belangsumerlang@gmail.com>
2024-08-22 10:57:40 +0700

    Coin Pickups

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
 
+# Called when the node enters the scene tree for the first time.
+
+
+
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	queue_free()
