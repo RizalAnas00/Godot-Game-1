e0d1a3d5654d27b77f979db033f4e118a448aca2
RizalAnas00 <belangsumerlang@gmail.com>
2024-08-23 08:05:38 +0700

    Dying 2.0

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -9,7 +9,7 @@ const SPEED = 60
 
 var direction = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(delta):
 	if ray_cast_right.is_colliding() or not ray_cast_below.is_colliding():
 		direction = -1
 		animated_sprite_2d.flip_h = true
